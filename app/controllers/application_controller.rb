4a510d668e44a2a574bc809736bd0a9d19e433a1
Sam <oyedeji.smt@gmail.com>
2024-12-02 22:23:59 -0600

    fixed admin and customer login session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :ensure_single_session
 
-  protected
+  private
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :address, :province ])
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :address, :province ])
+  def ensure_single_session
+    if admin_signed_in? && customer_signed_in?
+      sign_out(:customer) # Log out the customer when admin is logged in
+    elsif customer_signed_in? && admin_signed_in?
+      sign_out(:admin) # Log out the admin when customer is logged in
+    end
   end
 end
